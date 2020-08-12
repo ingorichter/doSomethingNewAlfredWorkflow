354c6afed8d07a83df49731bf608c4625392f76e
Ingo Richter <ingo.richter+github@gmail.com>
2020-08-12 10:36:16 +0200

    improve the iteration process to extract the data

--- tools/index.js
+++ tools/index.js
@@ -5,56 +5,53 @@ const puppeteer = require('puppeteer');
   const page = await browser.newPage();
   await page.goto('https://whats.new/shortcuts/', {waitUntil: 'networkidle2'});
 
-  const allResultsSelector = '.data-table__content';
-  await page.waitForSelector(allResultsSelector);
-
   // Wait for the results page to load and display the results.
   const categoryHeaderSelector = '.data-table__content .data-table__title';
   await page.waitForSelector(categoryHeaderSelector);
 
   // Extract the results from the page.
-  const serviceInfo = await page.evaluate((categoryHeaderSelector) => {
+  const serviceInfoJSON = await page.evaluate((categoryHeaderSelector) => {
     const categoryHeaders = Array.from(
       document.querySelectorAll(categoryHeaderSelector)
     );
 
+    var serviceTables = Array.from(
+      document.querySelectorAll('.data-table__table')
+    );
+
     const serviceNames2Services = Object.assign({});
-    categoryHeaders.forEach((categoryHeader) => {
+    categoryHeaders.forEach((categoryHeader, index) => {
       const title = categoryHeader.textContent.split('|')[0].trim();
       serviceNames2Services[categoryHeader.id] = Object.assign({
         categoryTitle: title,
         services: [],
       });
-    });
 
-    var categoriesTable = Array.from(
-      document.querySelectorAll('.data-table__table')
-    );
-
-    categoriesTable.forEach((categoryTable) => {
-      const categoryName = categoryTable.getAttribute('data-category-name');
       const servicesTableRows = Array.from(
-        categoryTable.querySelectorAll('tbody>.data-table__row')
+        serviceTables[index].querySelectorAll('tbody>.data-table__row')
       );
 
       servicesTableRows.forEach((serviceRow) => {
         // get all columns
         const columns = Array.from(serviceRow.querySelectorAll('td'));
         const serviceDetails = Object.assign({
-          domain: columns[0].textContent.trim().split(' '),
+          domain: columns[0].textContent
+            .trim()
+            .split(' ')
+            .filter((x) => x.length > 0),
           company: columns[1].textContent.trim(),
           description: columns[2].textContent.trim(),
         });
 
         // add this to the mapping from category to services
-        serviceNames2Services[categoryName].services.push(serviceDetails);
+        serviceNames2Services[categoryHeader.id].services.push(serviceDetails);
       });
     });
 
     return serviceNames2Services;
   }, categoryHeaderSelector);
 
-  console.log(JSON.stringify(serviceInfo));
+  console.log(JSON.stringify(serviceInfoJSON));
 
   await browser.close();
 })();
